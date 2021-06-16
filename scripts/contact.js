c55f2a17fa4204b04d5ffcfe1de6b4595cb9dbe1
Daniel Calvin <arockne2003@yahoo.com>
2021-06-16 10:19:10 -0700

    Refactor paragraph and heading elements to use createElementWithText

--- scripts/contact.js
+++ scripts/contact.js
@@ -14,17 +14,11 @@ const createElementWithText = function(element, text) {
 const home = document.body.querySelector('div.home');
 
 //Creation of heading element
-const heading = document.createElement('h2');
-const headingText = document.createTextNode('Contact Lunar Base');
-heading.appendChild(headingText);
+const heading = createElementWithText('h2', 'Contact Lunar Base');
 
 //creation of paragraph element
-const pElement = document.createElement('p');
 const contactDescription = 'Email us through our satelite based communications and we will get back to you as soon as your message has been relayed';
-const pTextNode = document.createTextNode(contactDescription);
-pElement.appendChild(pTextNode);
-
-//Contact button
+const description = createElementWithText('p', contactDescription);
 
 //create form element
 const contactForm = document.createElement('form');
@@ -74,7 +68,7 @@ contactForm.appendChild(fieldset);
 const formContainer = document.createElement('div');
 formContainer.setAttribute('class', 'contact');
 formContainer.appendChild(heading);
-formContainer.appendChild(pElement);
+formContainer.appendChild(description);
 formContainer.appendChild(contactForm);
 
 const contactButton = document.querySelector('.contactButton');
