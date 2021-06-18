20ae9f96cf91099bcea0895b3d40de2221a89e06
Daniel Calvin <arockne2003@yahoo.com>
2021-06-17 18:19:52 -0700

    Implement mainContent and refactor event listeners

--- scripts/events.js
+++ scripts/events.js
@@ -1,19 +1,20 @@
 window.addEventListener('load', (e) => {
+  
+  const mainContent = () => document.querySelector('main div');
+
   const main = document.querySelector('main');
   const home = document.body.querySelector('.home');
   const contact = completedContactContent();
+  
   const contactButton = document.querySelector('.contactButton');
-
   contactButton.addEventListener('click', function() {
-    const content = document.querySelector('main div');
-    content.remove();
+    mainContent().remove();
     main.appendChild(contact);
   })
   
   const homeButton = document.querySelector('.homeButton');
   homeButton.addEventListener('click', function() {
-    const content = document.querySelector('main div');
-    content.remove();
+    mainContent().remove();
     main.appendChild(home);
   })
 })
