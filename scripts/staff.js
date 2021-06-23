a0d4fd122b8a728b13b0a667e744a4203f99b18a
Daniel Calvin <arockne2003@yahoo.com>
2021-06-22 19:11:12 -0700

    Implement an example staff info

---
+++ scripts/staff.js
@@ -0,0 +1,7 @@
+const example = {
+  name: 'Daniel',
+  occupation: 'previous chef',
+  hobbies: ['coding', 'rowing', 'gaming'],
+  linkedIn: 'https://www.linkedin.com/in/daniel-calvin-301598170/',
+  github: 'https://github.com/Arockne'
+}
